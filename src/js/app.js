4b0839f30095aa40be74a4ddd62d9a7efbd023c1
jerryzhch <jerry_agboola@hotmail.com>
2021-10-23 20:46:38 +0200

    code upload

--- src/js/app.js
+++ src/js/app.js
@@ -35,7 +35,6 @@ var app = new Framework7({
   },
   card: {
     hideNavbarOnOpen: true,
-    closeByBackdropClick: true,
   },
   // App store
   store: store,
@@ -52,6 +51,7 @@ var app = new Framework7({
 
 function start() {
   const categoryForm = document.getElementById("category-form");
+  const resultCompanies = $(".result-companies");
   // load categories
   // let categories = ["Accounting", "Team Communication", "Time Tracking", "Billing", "Web ", "Inventory / Supply Chain", "Customer Database"];
   /*var result = chunkArray(categories, 4);
@@ -59,6 +59,8 @@ function start() {
   result.forEach((element) => {
     getCatRow(element, categoryForm);
   }); */
+  let selectedCounter = 0;
+  let selectedCats = [];
   app.request
     .json("http://localhost:5000/api/SupplierSearch/keyWords", null, (data) => {
       var result = chunkArray(data, 4);
@@ -72,28 +74,47 @@ function start() {
         if (e.target.classList.contains("button-fill")) {
           e.target.classList.remove("button-fill");
           e.target.classList.remove("selectedCat");
+          selectedCats.splice(selectedCats.indexOf(e.target.getAttribute("name")), 1);
+          selectedCounter--;
         } else {
           e.target.classList.add("button-fill");
           e.target.classList.add("selectedCat");
+          selectedCats.push(e.target.getAttribute("name"));
+          selectedCounter++;
         }
+        if (selectedCounter !== 0) {
+          $("#searchbar-autocomplete").hide();
+          //$(".page-background").css("overflow", "auto");
+        } else {
+          $("#searchbar-autocomplete").show();
+          //$(".page-background").css("overflow", "hidden");
+        }
+        if (selectedCats.length > 0) {
+          const concatString = selectedCats.reduce((a, b) => a + ";" + b);
+          app.request.json("http://localhost:5000/api/SupplierSearch/byKeyWords", "keyWords=" + concatString, (data) => {
+            data.forEach((d) => resultCompanies.append(getCompRow(d)));
+          });
+        }
+        resultCompanies.empty();
       });
     });
-
-  $(".start-search").on("click", (e) => {
-    let selectedCats = [];
-    [...$(".selectedCat")].map((el) => {
-      selectedCats.push(el.getAttribute("name"));
-    });
-    const resultsRow = document.querySelector(".results-row");
-
-    resultsRow.appendChild(getCompRow());
-    resultsRow.appendChild(getCompRow());
-    //const inputText = app.form.convertToData("#searchbar-autocomplete").searchQuery.toLowerCase();
-    const concatString = selectedCats.reduce((a, b) => a + ";" + b);
-    app.request.json("http://localhost:5000/api/SupplierSearch/byKeyWords", "keyWords=" + concatString, (data) => console.log(data));
-  });
   $(".searchQuery").keyup((e) => {
-    app.request.json("http://localhost:5000/api/SupplierSearch/keyWordsFromFreeText", "freeText=" + e.target.value.toLowerCase(), (data) => console.log(data));
+    if (e.target.value === "") {
+      resultCompanies.empty();
+      $(".catContainer").show();
+      //$(".page-background").css("overflow", "hidden");
+    } else {
+      $(".catContainer").hide();
+      //$(".page-background").css("overflow", "auto");
+      app.request.json("http://localhost:5000/api/SupplierSearch/byKeyWords", "keyWords=" + e.target.value.toLowerCase().replaceAll(" ", ";").replaceAll(".", ";").replaceAll(",", ";"), (data) => {
+        resultCompanies.empty();
+        data.forEach((d) => resultCompanies.append(getCompRow(d)));
+      });
+    }
+  });
+
+  app.request.json("http://localhost:5000/api/SupplierSearch/pollResult", "pollId=100", (data) => {
+    console.log(data);
   });
 }
 
@@ -103,9 +124,10 @@ function getCatRow(arr, toInsertIn) {
   arr.forEach((catName) => {
     const newDiv = cE("div");
     newDiv.setAttribute("class", "col category button button-outline");
-    newDiv.setAttribute("name", catName);
-    newDiv.innerHTML = catName;
+    newDiv.setAttribute("name", catName.keyword);
+    newDiv.innerHTML = catName.keyword;
     newP.appendChild(newDiv);
+    app.tooltip.create({ targetEl: newDiv, text: catName.description, offset: 2 });
   });
   toInsertIn.insertBefore(newP, toInsertIn.querySelector(".row"));
 }
@@ -126,7 +148,7 @@ function chunkArray(myArray, chunk_size) {
   return tempArray;
 }
 
-function getCompRow() {
+function getCompRow({ name, address, imageUrl, isTool, rating, keywords, url }) {
   const cardDiv = cE("div");
   const cardContent = cE("div");
   const bgColor = cE("div");
@@ -134,40 +156,57 @@ function getCompRow() {
   const cardClose = cE("a");
   const cardCloseI = cE("i");
   const cardContentPadding = cE("div");
-  const address = cE("p");
+  const addressP = cE("p");
+  const websiteP = cE("p");
   const starIcon = cE("i");
+  const houseIcon = cE("i");
   const imgLogo = cE("img");
+  const linkIcon = cE("i");
 
   cardDiv.setAttribute("class", "col-30 card card-expandable");
   cardContent.setAttribute("class", "card-content");
-  bgColor.setAttribute("class", "bg-color-red");
+  if (isTool) {
+    bgColor.setAttribute("class", "bg-color-yellow");
+  } else {
+    bgColor.setAttribute("class", "bg-color-red");
+  }
   bgColor.style = "height: 300px";
-  imgLogo.setAttribute("src", "https://www.websamurai.ch/wp-content/uploads/2018/10/WEBSAMURAI-mit-Claim-neg-500x134.png");
+  imgLogo.setAttribute("src", imageUrl);
   imgLogo.setAttribute("height", "50px");
   imgLogo.setAttribute("style", "padding: 10px 0 5px 10px");
   cardHeader.setAttribute("class", "card-header text-color-white display-block");
   starIcon.setAttribute("class", "icon f7-icons");
+  houseIcon.setAttribute("class", "icon f7-icons");
+  linkIcon.setAttribute("class", "icon f7-icons");
   cardClose.setAttribute("href", "#");
   cardClose.setAttribute("class", "link card-close card-opened-fade-in color-white");
   cardClose.setAttribute("style", "position: absolute; right: 15px; top: 15px");
   cardCloseI.setAttribute("class", "icon f7-icons");
   cardContentPadding.setAttribute("class", "card-content-padding");
-  address.innerHTML = "Technoparkstrasse 2, 8406 Winterthur";
   cardCloseI.innerHTML = "xmark_circle_fill";
   starIcon.innerHTML = "star_fill";
-  cardContentPadding.appendChild(address);
+  houseIcon.innerHTML = "house";
+  linkIcon.innerText = "link_circle";
+  if (!isTool) {
+    addressP.appendChild(houseIcon);
+    const newTextNode = document.createTextNode("  " + address);
+    addressP.appendChild(newTextNode);
+  }
+  const newA = cE("a");
+  newA.setAttribute("href", url);
+  newA.setAttribute("class", "link external");
+  newA.innerHTML = url;
+  websiteP.appendChild(linkIcon);
+  websiteP.appendChild(newA);
+  cardContentPadding.appendChild(addressP);
+  cardContentPadding.appendChild(websiteP);
   cardClose.appendChild(cardCloseI);
 
-  cardHeader.innerHTML = "Framework7 ";
-  const rating = 6;
+  cardHeader.innerHTML = name + " ";
   for (let i = 0; i < rating; i++) {
     cardHeader.appendChild(starIcon.cloneNode(true));
   }
-  const expertiseTuple = [
-    ["technik", "6"],
-    ["Acc", "4"],
-  ];
-  const expertises = getExpertisesElements(expertiseTuple, starIcon);
+  const expertises = getExpertisesElements(keywords, starIcon);
   expertises.forEach((expertise) => {
     cardHeader.innerHTML += "<br />";
     cardHeader.appendChild(expertise);
@@ -179,6 +218,8 @@ function getCompRow() {
   cardContent.appendChild(bgColor);
   cardContent.appendChild(cardContentPadding);
   cardDiv.appendChild(cardContent);
+
+  cardDiv.addEventListener("click", (e) => {});
   return cardDiv;
 }
 
@@ -187,7 +228,7 @@ function getExpertisesElements(exp, star) {
   exp.forEach((ex) => {
     const small1 = cE("small");
     small1.setAttribute("style", "opacity: 0.7");
-    small1.innerHTML = ex[0] + " ";
+    small1.innerHTML = ex[0].capitalize() + " ";
     for (let i = 0; i < ex[1] - 0; i++) {
       small1.appendChild(star.cloneNode(true));
     }
@@ -195,3 +236,9 @@ function getExpertisesElements(exp, star) {
   });
   return arrayOfRatedExp;
 }
+Object.defineProperty(String.prototype, "capitalize", {
+  value: function () {
+    return this.charAt(0).toUpperCase() + this.slice(1);
+  },
+  enumerable: false,
+});
